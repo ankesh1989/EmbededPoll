fb1e825a579591451f9e138cc373ded5f5b6e39b
ankesh1989 <ankesh1989@gmail.com>
2017-09-11 20:42:41 +0530

    Added
    Added

---
+++ POC/UseCodeEmbedHere/UserEmbed/UserEmbed/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UserEmbed.Startup))]
+namespace UserEmbed
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
